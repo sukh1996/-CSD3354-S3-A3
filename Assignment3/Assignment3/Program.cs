91c2a22637410b01057ca8b5fc23cb8664956da7
Sukhveer Kaur Brar <736051@student.tor.loc>
2019-07-10 09:13:28 -0400

    second commit

--- Assignment3/Assignment3/Program.cs
+++ Assignment3/Assignment3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,32 @@ namespace Assignment3
     {
         static void Main(string[] args)
         {
+          
+                var tp = new TextProcessing();
+                tp.Run();
+            
+        }
+    }
+    class TextProcessing
+    {
+        Queue<string> names = new Queue<string>();
+        public void Run()
+        {
+
+            // Open the names file and access the data
+            using (StreamReader file = new StreamReader("U:/Users/736051/Assignment3/Assignment3/Assignment3/names.txt"))
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    names.Enqueue(ln);
+                    counter++;
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter} lines.");
+            }
         }
     }
 }
